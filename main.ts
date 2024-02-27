3371891ed060177047bdabc7d5ca8d02749323e0
Veerpal Brar <veerpal707@gmail.com>
2024-02-27 17:52:50 -0500

    add modal to search notebox

--- main.ts
+++ main.ts
@@ -1,134 +1,76 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, requestUrl,  SuggestModal, normalizePath,  FileSystemAdapter } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+interface SearchResult {
+	content: string;
+	file: string;
+  }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+export class SearchModal extends SuggestModal<SearchResult> {
+  async getSuggestions(query: string): Promise<SearchResult[]> {
+	const ret: SearchResult[] = []
+	let basePath = getBasePath()
+	await requestUrl(`http://localhost:8070/search?txt=${query}`)
+		.then(function (response) {
+			response.json.result.map((result: SearchResult) => {
+				if (result["file"].includes(basePath)) {
+					ret.push({
+						content: result["content"],
+						file: removeBasePathFromFullFilePath(result['file'])
+					})
 				}
+			})
+			
+		})
+		.catch(function (error) {
+			console.log(error);
+			return []
+		});
+	return ret;
+  }
+
+  renderSuggestion(book: SearchResult, el: HTMLElement) {
+    el.createEl("div", { text: book.content });
+    el.createEl("small", { text: book.file });
+  }
+
+  onChooseSuggestion(book: SearchResult, evt: MouseEvent | KeyboardEvent) {
+    let filePath = normalizePath(book.file);
+	this.app.workspace.openLinkText(filePath, "");
+  }
+}
+
+
+export default class NoteboxSearch extends Plugin {
+	async onload() {
+		this.addRibbonIcon('dice', 'Notebox Search', (evt: MouseEvent) => {
+			new SearchModal(this.app).open();
+		});
+
+		this.addCommand({
+			id: 'open-notebox-search-modal',
+			name: 'Notebox Search',
+			callback: () => {
+				new SearchModal(this.app).open();
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function getBasePath() {
+	let basePath = '';
+	let adapter = app.vault.adapter;
+	if (adapter instanceof FileSystemAdapter) {
+		basePath =  adapter.getBasePath().replace("/", "") // remove first / from path
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	return basePath;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function removeBasePathFromFullFilePath(fullPath: string) {
+	let basePath = getBasePath()
+	return fullPath.replace(basePath, "").replace("/", "") // strips any / at begining
 }
